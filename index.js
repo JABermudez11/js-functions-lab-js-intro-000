4c4d3d057993063b7eb7edd96a9849ef02d9c854
Justin Bermudez <justinanbermudez@gmail.com>
2019-12-06 11:34:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(name, days) {
-  return(`It's ${days} days until ${holiday}!`)
+  return(`It's ${days} days until ${name}!`)
 }
